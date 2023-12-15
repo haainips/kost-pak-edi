5f876d1fa35bfa73f9683211d1624fb41be47925
Naverty <fatahanif@students.amikom.ac.id>
2023-12-15 09:59:25 +0700

    new version

--- database/migrations/2023_11_25_203956_create_rooms_table.php
+++ database/migrations/2023_11_25_203956_create_rooms_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('rooms', function (Blueprint $table) {
             $table->id();
+            $table->string('tipe');
             $table->string('gambar');
             $table->string('Fasilitas');
             $table->integer('harga');
